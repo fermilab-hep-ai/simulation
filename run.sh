8ab2e73d44fc8ccccc3f95d56d657f9551bd6045
egovorko <ekaterina.govorkova@cern.ch>
2024-02-20 17:18:11 +0100

    update run.sh to run from wherever

--- run.sh
+++ run.sh
@@ -11,71 +11,72 @@ proc=$1
 nevts=$2
 seed=$3
 outdir=$4
+simultaiondir=$5
 
 # locate run cards
-if [ ! -d ./processes/${proc} ]; then
-  echo ./processes/${proc} " does not exist!"
+if [ ! -d ${simultaiondir}/processes/${proc} ]; then
+  echo ${simultaiondir}/processes/${proc} " does not exist!"
   exit 1
 fi
 
-if [ ! -e ./processes/${proc}/${proc}_proc_card.dat ]; then
-  echo ./processes/${proc}/${proc}_proc_card.dat " does not exist!"
+if [ ! -e ${simultaiondir}/processes/${proc}/${proc}_proc_card.dat ]; then
+  echo ${simultaiondir}/processes/${proc}/${proc}_proc_card.dat " does not exist!"
   exit 1
 fi
 
-if [ ! -e ./processes/${proc}/${proc}_run_card.dat ]; then
-  echo ./processes/${proc}/${proc}_run_card.dat " does not exist!"
+if [ ! -e ${simultaiondir}/processes/${proc}/${proc}_run_card.dat ]; then
+  echo ${simultaiondir}/processes/${proc}/${proc}_run_card.dat " does not exist!"
   exit 1
 fi
 
 
 # run MadGraph event generation
 mkdir -p ${outdir}/${proc}/
-cp ./processes/${proc}/${proc}_proc_card.dat ${outdir}/${proc}/proc_card.dat
+cp ${simultaiondir}/processes/${proc}/${proc}_proc_card.dat ${outdir}/${proc}/proc_card.dat
 sed -i -e "s@_OUTDIR_@$outdir@g" ${outdir}/${proc}/proc_card.dat
 /usr/local/MG5_aMC_v3_5_1/bin/mg5_aMC ${outdir}/${proc}/proc_card.dat
 
 # write run cards
-cp ./cards/* ${outdir}/${proc}/Cards/
-cp ./processes/${proc}/${proc}_run_card.dat ${outdir}/${proc}/Cards/run_card.dat
-if [ -e ./processes/${proc}/${proc}_cuts.f ]; then
+cp ${simultaiondir}/cards/* ${outdir}/${proc}/Cards/
+cp ${simultaiondir}/processes/${proc}/${proc}_run_card.dat ${outdir}/${proc}/Cards/run_card.dat
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_cuts.f ]; then
   echo "copying custom cuts.f file"
-  cp ./processes/${proc}/${proc}_cuts.f ${outdir}/${proc}/SubProcesses/cuts.f
+  cp ${simultaiondir}/processes/${proc}/${proc}_cuts.f ${outdir}/${proc}/SubProcesses/cuts.f
 fi
 
-if [ -e ./processes/${proc}/${proc}_FKS_params.dat ]; then
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_FKS_params.dat ]; then
   echo "copying custom FKS_params.dat file"
-  cp ./processes/${proc}/${proc}_FKS_params.dat ${outdir}/${proc}/Cards/FKS_params.dat
+  cp ${simultaiondir}/processes/${proc}/${proc}_FKS_params.dat ${outdir}/${proc}/Cards/FKS_params.dat
 fi
 
-if [ -e ./processes/${proc}/${proc}_setscales.f ]; then
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_setscales.f ]; then
   echo "copying custom setscales.f file"
-  cp ./processes/${proc}/${proc}_setscales.f ${outdir}/${proc}/SubProcesses/setscales.f
+  cp ${simultaiondir}/processes/${proc}/${proc}_setscales.f ${outdir}/${proc}/SubProcesses/setscales.f
 fi
 
-if [ -e ./processes/${proc}/${proc}_reweight_xsec.f ]; then
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_reweight_xsec.f ]; then
   echo "copying custom reweight_xsec.f file"
-  cp ./processes/${proc}/${proc}_reweight_xsec.f ${outdir}/${proc}/SubProcesses/reweight_xsec.f
+  cp ${simultaiondir}/processes/${proc}/${proc}_reweight_xsec.f ${outdir}/${proc}/SubProcesses/reweight_xsec.f
 fi
 
-if [ -e ./processes/${proc}/${proc}_reweight_card.dat ]; then
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_reweight_card.dat ]; then
   echo "copying custom reweight file"
-  cp ./processes/${proc}/${proc}_reweight_card.dat ${outdir}/${proc}/Cards/reweight_card.dat
+  cp ${simultaiondir}/processes/${proc}/${proc}_reweight_card.dat ${outdir}/${proc}/Cards/reweight_card.dat
 fi
 
-if [ -e ./processes/${proc}/${proc}_param_card.dat ]; then
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_param_card.dat ]; then
   echo "copying custom params file"
-  cp ./processes/${proc}/${proc}_param_card.dat ${outdir}/${proc}/Cards/param_card.dat
+  cp ${simultaiondir}/processes/${proc}/${proc}_param_card.dat ${outdir}/${proc}/Cards/param_card.dat
 fi
-    
-if [ -e ./processes/${proc}/${proc}_madspin_card.dat ]; then
-  cp ./processes/${proc}/${proc}_madspin_card.dat ${outdir}/${proc}/Cards/madspin_card.dat
+
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_madspin_card.dat ]; then
+  cp ${simultaiondir}/processes/${proc}/${proc}_madspin_card.dat ${outdir}/${proc}/Cards/madspin_card.dat
   sed -i -e "s@_OUTDIR_@$outdir/$proc@g" ${outdir}/${proc}/Cards/madspin_card.dat
 fi
 
 echo "set nevents ${nevts}" >> ${outdir}/${proc}/Cards/launchrun.dat
-if [ -e ./processes/${proc}/${proc}_customizecards.dat ]; then
-        cat ./processes/${proc}/${proc}_customizecards.dat | sed '/^$/d;/^#.*$/d' >> ${outdir}/${proc}/Cards/launchrun.dat
+if [ -e ${simultaiondir}/processes/${proc}/${proc}_customizecards.dat ]; then
+        cat ${simultaiondir}/processes/${proc}/${proc}_customizecards.dat | sed '/^$/d;/^#.*$/d' >> ${outdir}/${proc}/Cards/launchrun.dat
         echo "" >> ${outdir}/${proc}/Cards/launchrun.dat
 fi
 echo "done" >> ${outdir}/${proc}/Cards/launchrun.dat
@@ -86,4 +87,4 @@ sed -i -e "s@_ISEED_@$seed@g" ${outdir}/${proc}/Cards/run_card.dat
 
 ${outdir}/${proc}/bin/madevent ${outdir}/${proc}/Cards/launchrun.dat
 # make validation plots
-python3 ./make_plots.py -p ${outdir}/${proc}/
+python3 ${simultaiondir}/make_plots.py -p ${outdir}/${proc}/
