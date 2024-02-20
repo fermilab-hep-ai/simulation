805682caa3009a9554a159c96e87eaf3f6e941d0
egovorko <ekaterina.govorkova@cern.ch>
2024-02-20 17:18:38 +0100

    update condor submission script; do not copy the whole siulation folder

--- submit_to_condor.py
+++ submit_to_condor.py
@@ -17,22 +17,28 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--toys', type=int, default=1,
         help='number of toys to be processed')
 
-    parser.add_argument('-n', '--nevents', type=int, default=1,
+    parser.add_argument('-n', '--nevents', type=int, default=1000,
         help='number of toys to be processed')
 
-    parser.add_argument('-p', '--process', type=str, default='QCD_HT50toInf',
+    parser.add_argument('-p', '--process', type=str, default='WJetsToLNu_13TeV-madgraphMLM-pythia8',
         help='name of the process to generate')
 
     parser.add_argument('--seed', default=None,
         help='seed for simulation')
 
+    # eosdir = '/eos/cms/store/cmst3/user/egovorko/simulation/'
+    parser.add_argument('--eosdir', default='./',
+        help='path to the shared simulation folder')
+
+    parser.add_argument('-l','--local', action='store_true',
+        help='if to be run locally')
+
     args = parser.parse_args()
 
-
     # folder to save the outputs of the sh script
-    mydir = f'{args.output}/'
-    os.system(f'mkdir -p {mydir}')
-    print(f'Created output directory {mydir}')
+    outputdir = f'{args.output}/'
+    os.system(f'mkdir -p {outputdir}')
+    print(f'Created output directory {outputdir}')
 
     # folder to save the outputs of each condor job (file.out, file.log, file.err)
     label = f'{args.output.split("/")[-1]}_{args.process}_{args.nevents}_{time.time()}'
@@ -42,34 +48,42 @@ if __name__ == '__main__':
     # change permission to the submission folder so that we could copy it
     os.system(f'chmod a+x {os.getcwd()}')
 
-    for i in range(args.toys):
+    if args.local:
+        os.system(f'apptainer exec \
+            --bind {outputdir}:/output \
+            --bind {args.eosdir}:/simulation \
+            --nv /cvmfs/unpacked.cern.ch/registry.hub.docker.com/jmduarte/mapyde:latest \
+            sh /simulation/{args.shscript} {args.process} {args.nevents} 42 /output /simulation\n')
+    else:
+        for i in range(args.toys):
 
-        # define job label and generation seed
-        joblabel = f'{label}/{i}'
-        seed = datetime.now().microsecond+datetime.now().second+datetime.now().minute \
-            if not args.seed else args.seed
+            # define job label and generation seed
+            joblabel = f'{label}/{i}'
+            seed = datetime.now().microsecond+datetime.now().second+datetime.now().minute \
+                if not args.seed else args.seed
 
-        # src file
-        script_src = open(f'{joblabel}.src', 'w')
-        script_src.write('#!/bin/bash\n')
-        script_src.write('apptainer shell -B /afs -B /eos --nv /cvmfs/unpacked.cern.ch/registry.hub.docker.com/jmduarte/mapyde:latest\n')
-        script_src.write('export MADGRAPH="/usr/local/MG5_aMC_v3_5_1/"\n')
-        script_src.write(f'cp -r {os.getcwd()} ./\n')
-        script_src.write(f'cd simulation \n')
-        script_src.write(f'sh {args.shscript} {args.process} {args.nevents} {seed} {mydir}\n')
-        script_src.close()
-        os.system(f'chmod a+x {joblabel}.src')
+            # src file
+            script_src = open(f'{joblabel}.src', 'w')
+            script_src.write('#!/bin/bash\n')
+            script_src.write(f'apptainer exec \
+                --bind {outputdir}:/output \
+                --bind {args.eosdir}:/simulation \
+                --nv /cvmfs/unpacked.cern.ch/registry.hub.docker.com/jmduarte/mapyde:latest \
+                sh /simulation/{args.shscript} {args.process} {args.nevents} {seed} /output /simulation\n')
 
-        # condor file
-        script_condor = open(f'{joblabel}.condor', 'w')
-        script_condor.write(f'executable = {joblabel}.src\n')
-        script_condor.write('universe = vanilla\n')
-        script_condor.write(f'output = {joblabel}.out\n')
-        script_condor.write(f'error = {joblabel}.err\n')
-        script_condor.write(f'log = {joblabel}.log\n')
-        script_condor.write('+MaxRuntime = 500000\n')
-        script_condor.write('queue\n')
-        script_condor.close()
+            script_src.close()
+            os.system(f'chmod a+x {joblabel}.src')
 
-        # condor file submission
-        os.system(f'condor_submit {joblabel}.condor')
+            # condor file
+            script_condor = open(f'{joblabel}.condor', 'w')
+            script_condor.write(f'executable = {joblabel}.src\n')
+            script_condor.write('universe = vanilla\n')
+            script_condor.write(f'output = {joblabel}.out\n')
+            script_condor.write(f'error = {joblabel}.err\n')
+            script_condor.write(f'log = {joblabel}.log\n')
+            script_condor.write('+MaxRuntime = 500000\n')
+            script_condor.write('queue\n')
+            script_condor.close()
+
+            # condor file submission
+            os.system(f'condor_submit {joblabel}.condor')
