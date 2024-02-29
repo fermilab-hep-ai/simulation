a9c017794908160d18de710f5dafb7b50fd7da09
ryanliu <liuryan30@gmail.com>
2024-02-29 17:41:55 -0600

    added a script to write job inputs

--- README.md
+++ README.md
@@ -10,12 +10,10 @@ wget https://cernbox.cern.ch/remote.php/dav/public-files/IyG0C0tfkXW7ifF/MinBias
 ```
 Finally, run the `run.sh` command with the docker image: (you can also build the image using the dockerfile under `/docker`)
 ```
-apptainer exec --bind $outdir,$simdir /cvmfs/unpacked.cern.ch/registry.hub.docker.com/jmduarte/mapyde:latest sh {simdir}/run.sh $process $nevents $seed $outdir $simdir
+apptainer exec --bind $outdir --nv /cvmfs/unpacked.cern.ch/registry.hub.docker.com/jmduarte/mapyde:latest sh run.sh $process $nevents $seed $outdir
 ```
-`$outdir` should NOT contain the process name you want to run. A folder will be automatically created for you. `$simdir` should be the directory where the simulation repository is. Note that it is a known bug that if `$outdir` and `$simdir` are both on eos, OS Error 35 can be triggered and should be avoided 
-
-Alternatively, use the `submit_to_condor.py`:
+Or alternatively, use the `submit.sh`:
 ```
-python3 submit_to_condor.py -o $outdir -p $proc --simdir $simdir --seed $seed -n $nevents
+sh submit.sh $process $nevents $seed $outdir
 ```
-Set the `-l` flag if you want to run locally, otherwise it will be submitted as a condor job.
+Note that `$outdir` should NOT contain the process name you want to run. A folder will be automatically created for you.
--- run.sh
+++ run.sh
@@ -12,8 +12,6 @@ mkdir -p $outdir/tmp-${proc}-${nevts}-${seed}
 cd $outdir/tmp-${proc}-${nevts}-${seed}
 
 workdir=$(pwd)
-proc_outdir="${outdir}/${proc}-${nevts}-${seed}"
-echo $proc_outdir
 
 echo "workdir: $workdir"
 echo "args: $@"
@@ -36,63 +34,62 @@ fi
 
 
 # run MadGraph event generation
-mkdir -p ${proc_outdir}/
-rm -r ${proc_outdir}/*
-cp ${simdir}/processes/${proc}/${proc}_proc_card.dat ${proc_outdir}/proc_card.dat
-sed -i -e "s@_OUTDIR_@$proc_outdir@g" ${proc_outdir}/proc_card.dat
-/usr/local/MG5_aMC_v3_5_1/bin/mg5_aMC ${proc_outdir}/proc_card.dat
+mkdir -p ${outdir}/${proc}/
+cp ${simdir}/processes/${proc}/${proc}_proc_card.dat ${outdir}/${proc}/proc_card.dat
+sed -i -e "s@_OUTDIR_@$outdir@g" ${outdir}/${proc}/proc_card.dat
+/usr/local/MG5_aMC_v3_5_1/bin/mg5_aMC ${outdir}/${proc}/proc_card.dat
 
 # write run cards
-cp ${simdir}/cards/* ${proc_outdir}/Cards/
-cp ${simdir}/processes/${proc}/${proc}_run_card.dat ${proc_outdir}/Cards/run_card.dat
+cp ${simdir}/cards/* ${outdir}/${proc}/Cards/
+cp ${simdir}/processes/${proc}/${proc}_run_card.dat ${outdir}/${proc}/Cards/run_card.dat
 if [ -e ${simdir}/processes/${proc}/${proc}_cuts.f ]; then
   echo "copying custom cuts.f file"
-  cp ${simdir}/processes/${proc}/${proc}_cuts.f ${proc_outdir}/SubProcesses/cuts.f
+  cp ${simdir}/processes/${proc}/${proc}_cuts.f ${outdir}/${proc}/SubProcesses/cuts.f
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_FKS_params.dat ]; then
   echo "copying custom FKS_params.dat file"
-  cp ${simdir}/processes/${proc}/${proc}_FKS_params.dat ${proc_outdir}/Cards/FKS_params.dat
+  cp ${simdir}/processes/${proc}/${proc}_FKS_params.dat ${outdir}/${proc}/Cards/FKS_params.dat
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_setscales.f ]; then
   echo "copying custom setscales.f file"
-  cp ${simdir}/processes/${proc}/${proc}_setscales.f ${proc_outdir}/SubProcesses/setscales.f
+  cp ${simdir}/processes/${proc}/${proc}_setscales.f ${outdir}/${proc}/SubProcesses/setscales.f
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_reweight_xsec.f ]; then
   echo "copying custom reweight_xsec.f file"
-  cp ${simdir}/processes/${proc}/${proc}_reweight_xsec.f ${proc_outdir}/SubProcesses/reweight_xsec.f
+  cp ${simdir}/processes/${proc}/${proc}_reweight_xsec.f ${outdir}/${proc}/SubProcesses/reweight_xsec.f
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_reweight_card.dat ]; then
   echo "copying custom reweight file"
-  cp ${simdir}/processes/${proc}/${proc}_reweight_card.dat ${proc_outdir}/Cards/reweight_card.dat
+  cp ${simdir}/processes/${proc}/${proc}_reweight_card.dat ${outdir}/${proc}/Cards/reweight_card.dat
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_param_card.dat ]; then
   echo "copying custom params file"
-  cp ${simdir}/processes/${proc}/${proc}_param_card.dat ${proc_outdir}/Cards/param_card.dat
+  cp ${simdir}/processes/${proc}/${proc}_param_card.dat ${outdir}/${proc}/Cards/param_card.dat
 fi
 
 if [ -e ${simdir}/processes/${proc}/${proc}_madspin_card.dat ]; then
-  cp ${simdir}/processes/${proc}/${proc}_madspin_card.dat ${proc_outdir}/Cards/madspin_card.dat
-  sed -i -e "s@_OUTDIR_@$proc_outdir@g" ${proc_outdir}/Cards/madspin_card.dat
+  cp ${simdir}/processes/${proc}/${proc}_madspin_card.dat ${outdir}/${proc}/Cards/madspin_card.dat
+  sed -i -e "s@_OUTDIR_@$outdir/$proc@g" ${outdir}/${proc}/Cards/madspin_card.dat
 fi
 
-echo "set nevents ${nevts}" >> ${proc_outdir}/Cards/launchrun.dat
+echo "set nevents ${nevts}" >> ${outdir}/${proc}/Cards/launchrun.dat
 if [ -e ${simdir}/processes/${proc}/${proc}_customizecards.dat ]; then
-        cat ${simdir}/processes/${proc}/${proc}_customizecards.dat | sed '/^$/d;/^#.*$/d' >> ${proc_outdir}/Cards/launchrun.dat
-        echo "" >> ${proc_outdir}/Cards/launchrun.dat
+        cat ${simdir}/processes/${proc}/${proc}_customizecards.dat | sed '/^$/d;/^#.*$/d' >> ${outdir}/${proc}/Cards/launchrun.dat
+        echo "" >> ${outdir}/${proc}/Cards/launchrun.dat
 fi
-echo "done" >> ${proc_outdir}/Cards/launchrun.dat
+echo "done" >> ${outdir}/${proc}/Cards/launchrun.dat
 
-sed -i -e "s@_NEVENTS_@$nevts@g" ${proc_outdir}/Cards/run_card.dat
-sed -i -e "s@_PileUpFile_@$simdir/MinBias_100k.pileup@g" ${proc_outdir}/Cards/delphes_card.dat
-sed -i -e "s@_ISEED_@$seed@g" ${proc_outdir}/Cards/run_card.dat
+sed -i -e "s@_NEVENTS_@$nevts@g" ${outdir}/${proc}/Cards/run_card.dat
+sed -i -e "s@_PileUpFile_@$simdir/MinBias_100k.pileup@g" ${outdir}/${proc}/Cards/delphes_card.dat
+sed -i -e "s@_ISEED_@$seed@g" ${outdir}/${proc}/Cards/run_card.dat
 
-${proc_outdir}/bin/madevent ${proc_outdir}/Cards/launchrun.dat
+${outdir}/${proc}/bin/madevent ${outdir}/${proc}/Cards/launchrun.dat
 # make validation plots
-python3 ${simdir}/make_plots.py -p ${proc_outdir}/ -v ${simdir}/validation_config.yaml
+python3 ${simdir}/make_plots.py -p ${outdir}/${proc}/ -v ${simdir}/validation_config.yaml
 # clean up
 rm -r $outdir/tmp-${proc}-${nevts}-${seed}
