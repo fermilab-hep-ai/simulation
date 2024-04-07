c4008663f17f67877b297297f39e84039a22ce1f
Ryan Liu <ryliu@lxplus967.cern.ch>
2024-04-08 00:36:50 +0200

    modified condor directories

--- submit_to_condor.py
+++ submit_to_condor.py
@@ -8,13 +8,12 @@ def submit_to_condor(process, nevents, seed, outdir, simdir, istest):
     binding_paths = os.path.commonpath([outdir, simdir])
     if binding_paths == "/":
         binding_paths = f"{outdir},{simdir}"
-    print(f"Binding {binding_paths} to the container.")
 
     # folder to save the outputs of each condor job (file.out, file.log, file.err)
-    label = f'{process}_{nevents}_{seed}'
+    label = os.path.join(outdir, f'{process}_{nevents}_{seed}', "condor", )
     os.system(f'mkdir -p {label}')
-    print(f'Created Condor output directory {label}')
-    label = f'{simdir}/{label}/{label}'
+    print(f'Created Condor output directory {process}_{nevents}_{seed}')
+    
     # src file
     script_src = open(f'{label}.src', 'w')
     script_src.write('#!/bin/bash\n')
