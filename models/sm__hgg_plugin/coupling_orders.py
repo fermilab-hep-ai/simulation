ab0ebed0811d748c11dfc6713fd4b37aa8b5820b
Javier Duarte <jduarte@ucsd.edu>
2025-05-21 06:57:12 -0700

    first attempt

---
+++ models/sm__hgg_plugin/coupling_orders.py
@@ -0,0 +1,22 @@
+
+# This file was automatically created by The UFO_usermod        
+from object_library import all_orders, CouplingOrder
+QCD = CouplingOrder(expansion_order = 99,
+                    hierarchy = 1,
+                    name = 'QCD')
+
+
+QED = CouplingOrder(expansion_order = 99,
+                    hierarchy = 2,
+                    name = 'QED')
+
+
+HIG = CouplingOrder(expansion_order = 1,
+                    hierarchy = 2,
+                    name = 'HIG')
+
+
+HIW = CouplingOrder(expansion_order = 1,
+                    hierarchy = 2,
+                    name = 'HIW')
+
