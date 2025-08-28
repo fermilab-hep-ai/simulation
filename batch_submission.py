61f8b66a62fabe5943da1b3d631cb8861b2460f2
pploner <philip@ploner.ch>
2025-08-28 15:49:00 +0200

    easy interface to submit one batch of jobs for production

---
+++ batch_submission.py
@@ -0,0 +1,19 @@
+import cmd
+from glob import glob
+import os
+
+high_performance = True
+job_files = glob("job_csvs/batch20/*.csv")
+
+for job_file in job_files:
+    print(f"Processing {job_file}")
+    job_name = os.path.splitext(os.path.basename(job_file))[0]
+    if high_performance:
+        cmd = f"python3 submit_to_slurm.py -o /p/scratch/laionize/ploner1/full_production_1percent/{job_name} -csv {job_file} --simdir /p/home/jusers/ploner1/juwels/ploner1_juwels/simulation -l {job_name} -hp"
+    else:
+        cmd = f"python3 submit_to_slurm.py -o /p/scratch/laionize/ploner1/full_production_1percent/{job_name} -csv {job_file} --simdir /p/home/jusers/ploner1/juwels/ploner1_juwels/simulation -l {job_name}"
+    result = os.system(cmd)
+    if result != 0:
+        print(f"Error processing {job_file}")
+    else:
+        print(f"Successfully submitted {job_name}")
